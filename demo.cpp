affcaeddedb67ff5c526fd9958a9cc475b3778ad
张翼翔 <21371055@buaa.edu.cn>
2022-09-19 22:49:04 +0800

    this is the first edition

---
+++ demo.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 10791 on 2022/9/19.
+//
+
+#include "demo.h"
+#include<cstdio>
+
+namespace std {
+} // std
+
+
+int main()
+{
+    freopen("test.txt", "r", stdin);
+    printf("Hello world!");
+
+
+
+
+
+
+
+    fclose(stdin); //close "test.txt"
+    return 0; //test
+}
